e4b44624d6e348e0e351df8915ae3124ee2aee30
onadegibert <onadegibert@gmail.com>
2020-06-25 09:31:38 +0200

    First init

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+### Google Spreadsheets API
+Go to google.sheets and enable the API, create service account credentials and save them in the credentials.json.
+Share the document you want to read from with your client_email.
---
+++ create_metadata.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import gspread
+import json
+
+
+def open_spreadsheet(sheet_name):
+    # Edit your credentials and share the spreadsheet with your client_email from the credentials fle
+    gc = gspread.service_account(filename='credentials.json')
+    # spreadsheet = gc.open("Metadata (respostes)")
+    spreadsheet = gc.open("Data versioning proposal (respostes)")
+    sheet = spreadsheet.worksheet(sheet_name)
+    return sheet
+
+
+def get_values(sheet_name, range_cells, drop_columns=list()):
+    sheet = open_spreadsheet(sheet_name)
+    data = sheet.get(range_cells)
+    df = pd.DataFrame(data[1:], columns=data[0])
+    df.drop(drop_columns, axis=1, inplace=True)
+    df = df.apply(lambda x: x.astype(str).str.lower())
+    return df
+
+
+def get_actions(state_entry):
+    actions = list()
+    for index in range(int(len(state_entry) / 5)):
+        index = str(index + 1)
+        action_entry = dict()
+        action_entry['name'] = state_entry['name_' + index]
+        action_entry['src'] = state_entry['src_' + index]
+        action_entry['tgt'] = state_entry['tgt_' + index]
+        action_entry['script'] = state_entry['script_' + index]
+        action_entry['order'] = state_entry['order_' + index]
+        actions.append(action_entry)
+    return actions
+
+
+def get_corpus_version(version, date, path, states):
+    corpus_version = {'version': version, 'date': date, 'path': path}
+    if len(states) > 0:
+        corpus_version['corpus_version_states'] = states
+    return (corpus_version)
+
+
+def get_actions(state_entry):
+    actions = []
+    for index in range(int((len(state_entry) - 3) / 5)):  # remove three positions: corpus_version, name, date
+        index = str(index + 1)
+        action_entry = dict()
+        action_entry['name'] = state_entry['name_' + index]
+        action_entry['src'] = state_entry['src_' + index]
+        action_entry['tgt'] = state_entry['tgt_' + index]
+        action_entry['script'] = state_entry['script_' + index]
+        action_entry['order'] = state_entry['order_' + index]
+        actions.append(action_entry)
+    return actions
+
+
+def get_state(state_entry,version_path):
+    new_path = version_path + '_' + state_entry['state_name'] + '_' + state_entry['date']
+    clean_state_entry = dict(state=state_entry['state_name'], date=state_entry['date'], path=new_path, release=state_entry['release'])
+    actions = get_actions(state_entry)
+    clean_state_entry['actions'] = actions
+    return clean_state_entry
+
+
+def process_corpora(entry):
+    entry['corpus_versions'] = []
+    path = entry['dir_name'] + '_' + entry['version']
+    entry['corpus_versions'].append(get_corpus_version(entry['version'], entry['date'], path, []))
+    entry.pop('version')
+    entry.pop('date')
+    return entry
+
+
+def main():
+    corpora_drop_columns = ['scale', 'domain', 'document-level', 'cleaning',
+                            'sentence splitting and tokenizer']
+    corpora = get_values('Corpora', 'B:R', corpora_drop_columns)
+    versions = get_values('Corpus versions', 'T:V')
+    states = get_values('States', 'W:AO')
+    dict_corpora = corpora.to_dict(orient='records')
+    dict_versions = versions.to_dict(orient='records')
+    dict_states = states.to_dict(orient='records')
+
+    data_to_write = {}
+    # Process corpora sheet
+    for c_entry in dict_corpora:
+        c_entry = process_corpora(c_entry)
+
+        # Process corpus versions sheet
+        for v_entry in dict_versions:
+            if v_entry['pretty_name'] == c_entry['pretty_name']:
+
+                # Process corpus states sheet
+                states_list = []
+                corpus_version = v_entry['pretty_name'] + ' ' + v_entry['version']
+                path = c_entry['dir_name'] + '_' + v_entry['version']
+                for s_entry in dict_states:
+                    # Fix date format
+                    date = s_entry['date']
+                    if "/" in date:
+                        s_entry['date'] = date[-4:] + date[3:5] + date[:2]
+                    # Remove None values
+                    s_entry = {k: v for k, v in s_entry.items() if v != 'none'}
+                    if s_entry['corpus_version'] == corpus_version:  #TODO: add corpus versions from when you initiate a corpus
+                        states_list.append(get_state(s_entry,path))
+
+                c_entry['corpus_versions'].append(
+                    get_corpus_version(v_entry['version'], v_entry['date'], path, states_list))
+
+    with open('metadata.json', 'w', encoding='utf-8') as metadata:
+        json.dump(dict_corpora, metadata, indent=4, ensure_ascii=False)
+
+
+main()
